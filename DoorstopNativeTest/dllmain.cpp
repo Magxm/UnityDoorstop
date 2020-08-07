c530b2db8947fc7c20d769ff7494d67ec4dc4098
Magxm <19425588+Magxm@users.noreply.github.com>
2020-08-07 21:09:48 +0200

    Made DoorstopNativeTest leave proper alive signs

--- DoorstopNativeTest/dllmain.cpp
+++ DoorstopNativeTest/dllmain.cpp
@@ -1,6 +1,23 @@
 ﻿// dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <cstdio>
+#include <fstream>
 
+void aliveSign()
+{
+	std::ofstream aliveFile;
+	aliveFile.open("doorstop_native_is_alive.txt");
+	aliveFile << "ALIVE!";
+	aliveFile.close();
+
+	AllocConsole();
+	FILE* fDummy;
+	freopen_s(&fDummy, "CONIN$", "r", stdin);
+	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+
+	printf("Native Doorstep code loaded! \n");
+}
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
@@ -9,12 +26,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		AllocConsole();
-		FILE* fDummy;
-		freopen_s(&fDummy, "CONIN$", "r", stdin);
-		freopen_s(&fDummy, "CONOUT$", "w", stdout);
-
-		printf("Native Doorstep code loaded! \n");
+		aliveSign();
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
